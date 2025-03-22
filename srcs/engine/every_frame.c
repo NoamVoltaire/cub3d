23c38fa9133681737501b8715f0bf964e2b72418
Noam VOLTAIRE <noam.voltaire@gmail.com>
2025-03-22 14:55:42 +0100

    every frame*

--- srcs/engine/every_frame.c
+++ srcs/engine/every_frame.c
@@ -21,9 +21,13 @@ int	every_frame(void *param)
 		* (vars->bits_per_pixel / 8));
 	draw_map(vars->map, vars);
 	update_pos(&vars->player);
+	draw_rays(vars->player, vars->map, vars);
 	// set_keyb(&vars->player.keybind);
 	// printf("forward = %d, backward = %d, left = %d, right = %d\n", vars->player.keybind.forwards, vars->player.keybind.backwards, vars->player.keybind.left, vars->player.keybind.right);
 	printf("vars.player.xpos = %f\n", vars->player.xpos);
+	printf("vars.player.ypos = %f\n", vars->player.ypos);
+	printf("vars.player.angle = %f\n", vars->player.angle);
+	
 	draw_cube_player(vars->player, vars);
 	mlx_put_image_to_window(vars->mlx, vars->window, vars->img, 0, 0);
 	return (0);
