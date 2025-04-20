2c0d9c30b559aaa6179955047a2474f1b156c0b8
Noam VOLTAIRE <noam.voltaire@gmail.com>
2025-04-20 18:47:37 +0200

    dirty graphics, and screen tearing

--- srcs/engine/raycasting/draw_graphics.c
+++ srcs/engine/raycasting/draw_graphics.c
@@ -13,75 +13,18 @@ static void	ray_to_line(int r_nb, int l_height, t_vars *vars, t_ray ray)
 	unsigned int			t_color;
 	int				bytes_per_pixel;
 	char			*dst;
-	unsigned int	color_array[21];
 	//int	row_sz = vars->line_length / 2;
 	t_step = 32.0 / l_height;
 		x_step = ((32.0 / l_height) + 0.01);
 	if (l_height > (HEIGHT / 2))
 		l_height = HEIGHT / 2;	
 	int	image_size = vars->line_length * (HEIGHT / 2);
-
-	//printf("VARSINLEN ROW SZ  = %dn", row_sz);
-	//printf("ROW SIZE = %d\n", row_sz / 60);
 	bytes_per_pixel = vars->bits_per_pixel / 8;
 	y = (HEIGHT / 3) - l_height / 2;
-
-	// Prepare a block of 8 pixels with the same color
-	if (ray.hit_dir)
-	{
-	color_array[0] = 0x000000FF;
-	color_array[1] = 0x000000FF;
-	color_array[2] = 0x000000FF;
-	color_array[3] = 0x000000FF;
-	color_array[4] = 0x000000FF;
-	color_array[5] = 0x000000FF;
-	color_array[6] = 0x000000FF;
-	color_array[7] = 0x000000FF;
-	color_array[8] = 0x000000FF;
-	color_array[9] = 0x000000FF;
-	color_array[10] = 0x000000FF;
-	color_array[11] = 0x000000FF;
-	color_array[12] = 0x000000FF;
-	color_array[13] = 0x000000FF;
-	color_array[14] = 0x000000FF;
-	color_array[15] = 0x000000FF;
-	color_array[16] = 0x000000FF;
-	color_array[17] = 0x000000FF;
-	color_array[18] = 0x000000FF;
-	color_array[19] = 0x000000FF;
-	color_array[20] = 0x000000FF;
-	}
-	else
-	{
-	color_array[0] = 2639570;
-	color_array[1] = 2639570;
-	color_array[2] = 2639570;
-	color_array[3] = 2639570;
-	color_array[4] = 2639570;
-	color_array[5] = 2639570;
-	color_array[6] = 2639570;
-	color_array[7] = 2639570;
-	color_array[8] = 2639570;
-	color_array[9] = 2639570;
-	color_array[10] = 2639570;
-	color_array[11] = 2639570;
-	color_array[12] = 2639570;
-	color_array[13] = 2639570;
-	color_array[14] = 2639570;
-	color_array[15] = 2639570;
-	color_array[16] = 2639570;
-	color_array[17] = 2639570;
-	color_array[18] = 2639570;
-	color_array[19] = 2639570;
-	color_array[20] = 2639570;
-	}
-
 	// Get pointer to first pixel in the image buffer
 	dst = vars->addr + (y * vars->line_length) + (r_nb * 10 *bytes_per_pixel);
 
 	// CHANGE COLOR FROM TEXTURE
-	//t_color = *(unsigned int *)(vars->textures.t_addr + (ti * vars->line_length));
-	// Draw vertical strip using memcpy
 	i = 0;
 	while (i < l_height) 
 	{
@@ -89,13 +32,10 @@ static void	ray_to_line(int r_nb, int l_height, t_vars *vars, t_ray ray)
 		xif = (int)(ray.rx / 4.0) % 32;
 xif = (int)((ray.rx / 128.0) * 32) % 32;
 		//x_step = ((32.0 / l_height) - 0.001);
-		//t_color = *(unsigned int *)(vars->textures.t_addr + ((ti * 32 * 8) + (1 * bytes_per_pixel)));
-		//t_color = *(unsigned int *)(vars->textures.t_addr + (ti * 32 * bytes_per_pixel) + ((int)xif * bytes_per_pixel));
 		if (dst < vars->addr || dst + (10 * bytes_per_pixel) > vars->addr + image_size)
 			break;
 		while (j++ < 10)
 		{
-			//*(unsigned int *) dst = 2639570;
 		t_color = *(unsigned int *)(vars->textures.t_addr + (ti * 32 * bytes_per_pixel) + ((int)xif * bytes_per_pixel));
 			*(unsigned int *) dst = t_color;
 			dst += bytes_per_pixel;
