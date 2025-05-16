4b762b9e2a4a56311344b092a9e1d8468d56472d
Leonard Grellier <lgrellie@student.42.fr>
2025-05-16 17:30:32 +0200

    Add new main, old main -> main_loop.c

--- main.c
+++ main.c
@@ -3,24 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: noam <noam@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: lgrellie <lgrellie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/15 15:13:33 by noam              #+#    #+#             */
-/*   Updated: 2025/03/21 17:18:27 by noam             ###   ########.fr       */
+/*   Created: 2025/05/14 13:28:08 by lgrellie          #+#    #+#             */
+/*   Updated: 2025/05/16 17:07:41 by lgrellie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cub.h>
+#include "cub.h"
 
-int	main(void)
+int	check_map_name(char *str)
 {
+	size_t	len;
+
+	len = ft_strlen(str);
+	if (len < 5)
+		return (1);
+	return (ft_strcmp(&str[len - 4], ".cub"));
+}
+
+int	main(int ac, char **av)
+{
+	int		fd;
 	t_vars	vars;
 
-	init_vars(&vars);
-	the_hooks(&vars);
-	//if (!vars.textures.t_addr)  // Exit function early to prevent crash
-		 //printf("ITSNULLLLLLLLLL");
-		// printf("vars.player.xpos = %f\n", vars.player.xpos);
-	mlx_loop_hook(vars.mlx, every_frame, &vars);
-	mlx_loop(vars.mlx);
+	if (ac != 2 || check_map_name(av[1]))
+	{
+		printf("Syntax: ./cub3D [map_name].cub\n");
+		return (1);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		perror("cub3D");
+	if (parse_into_vars(fd, &vars))
+		return (1);
+	return (main_loop(vars));
 }
---
+++ main_loop.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_loop.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: noam <noam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/15 15:13:33 by noam              #+#    #+#             */
+/*   Updated: 2025/05/16 17:16:46 by lgrellie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cub.h>
+
+int	main_loop(t_vars vars)
+{
+	init_vars(&vars);
+	the_hooks(&vars);
+	//if (!vars.textures.t_addr)  // Exit function early to prevent crash
+		 //printf("ITSNULLLLLLLLLL");
+		// printf("vars.player.xpos = %f\n", vars.player.xpos);
+	mlx_loop_hook(vars.mlx, every_frame, &vars);
+	mlx_loop(vars.mlx);
+	return (0);
+}
